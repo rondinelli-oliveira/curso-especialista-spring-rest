99edf5aa992e32fc9de8c6d3b4559d3c862671f8
rondinelli-oliveira <rondinelliads@gmail.com>
2020-06-27 21:55:37 -0300

    Adicionado anotação @Repository.

--- food-api/src/main/java/com/food/infrastructure/repository/PermissaoRepositoryImpl.java
+++ food-api/src/main/java/com/food/infrastructure/repository/PermissaoRepositoryImpl.java
@@ -6,12 +6,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import com.food.domain.model.Permissao;
 import com.food.domain.repository.PermissaoRepository;
 
-@Component
+@Repository
 public class PermissaoRepositoryImpl implements PermissaoRepository {
 
     @PersistenceContext
