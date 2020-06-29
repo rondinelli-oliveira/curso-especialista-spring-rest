7986412b2be9140557c76e620cce31b6fba50b2a
rondinelli-oliveira <rondinelliads@gmail.com>
2020-06-29 19:48:31 -0300

    Implementando um repositório SDJ customizado.

---
+++ food-api/src/main/java/com/food/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,34 @@
+package com.food.infrastructure.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import com.food.domain.model.Restaurante;
+import com.food.domain.repository.RestauranteRepositoryQueries;
+
+@Repository
+public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	@Override
+	public List<Restaurante> find(String nome, 
+			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+		
+		var jpql = "from Restaurante where nome like :nome "
+				+ "and taxaFrete between :taxaInicial and :taxaFinal";
+		
+		return manager.createQuery(jpql, Restaurante.class)
+				.setParameter("nome", "%" + nome + "%")
+				.setParameter("taxaInicial", taxaFreteInicial)
+				.setParameter("taxaFinal", taxaFreteFinal)
+				.getResultList();
+	}
+	
+}
