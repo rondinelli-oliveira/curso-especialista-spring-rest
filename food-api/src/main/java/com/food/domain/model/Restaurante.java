295eac06b3d5c4840407c8987eeec4128dd11fe5
rondinelli-oliveira <rondinelliads@gmail.com>
2020-07-11 21:02:54 -0300

    Adicionando dados de testes com callback do Flyway.

--- food-api/src/main/java/com/food/domain/model/Restaurante.java
+++ food-api/src/main/java/com/food/domain/model/Restaurante.java
@@ -43,7 +43,7 @@ public class Restaurante {
 	private BigDecimal taxaFrete;
 	
 //	@JsonIgnoreProperties("hibernateLazyInitializer")
-	@JsonIgnore
+//	@JsonIgnore
 	@ManyToOne //(fetch = FetchType.LAZY)
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
@@ -62,7 +62,7 @@ public class Restaurante {
 	@Column(nullable = false, columnDefinition = "datetime")
 	private LocalDateTime dataAtualizacao;
 	
-//	@JsonIgnore
+	@JsonIgnore
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "restaurante_forma_pagamento",
 			joinColumns = @JoinColumn(name = "restaurante_id"),
