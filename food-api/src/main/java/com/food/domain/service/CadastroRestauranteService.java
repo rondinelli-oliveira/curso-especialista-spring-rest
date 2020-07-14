f1490f8fcb71c0c506d01b289b9c534b4a957d1b
rondinelli-oliveira <rondinelliads@gmail.com>
2020-07-13 21:02:57 -0300

    Desafio: refatorando os serviços REST.

--- food-api/src/main/java/com/food/domain/service/CadastroRestauranteService.java
+++ food-api/src/main/java/com/food/domain/service/CadastroRestauranteService.java
@@ -6,28 +6,34 @@ import org.springframework.stereotype.Service;
 import com.food.domain.exception.EntidadeNaoEncontradaException;
 import com.food.domain.model.Cozinha;
 import com.food.domain.model.Restaurante;
-import com.food.domain.repository.CozinhaRepository;
 import com.food.domain.repository.RestauranteRepository;
 
 @Service
 public class CadastroRestauranteService {
 
+	private static final String MSG_RESTAURANTE_NAO_ENCONTRADO 
+		= "Não existe um cadastro de restaurante com código %d";
+	
 	@Autowired
 	private RestauranteRepository restauranteRepository;
 	
 	@Autowired
-	private CozinhaRepository cozinhaRepository;
+	private CadastroCozinhaService cadastroCozinha;
 	
 	public Restaurante salvar(Restaurante restaurante) {
 		Long cozinhaId = restaurante.getCozinha().getId();
 		
-		Cozinha cozinha = cozinhaRepository.findById(cozinhaId)
-			.orElseThrow(() -> new EntidadeNaoEncontradaException(
-					String.format("Não existe cadastro de cozinha com código %d", cozinhaId)));
+		Cozinha cozinha = cadastroCozinha.buscarOuFalhar(cozinhaId);
 		
 		restaurante.setCozinha(cozinha);
 		
 		return restauranteRepository.save(restaurante);
+	}
+	
+	public Restaurante buscarOuFalhar(Long cidadeId) {
+		return restauranteRepository.findById(cidadeId)
+			.orElseThrow(() -> new EntidadeNaoEncontradaException(
+					String.format(MSG_RESTAURANTE_NAO_ENCONTRADO, cidadeId)));
 	}
 	
 }
