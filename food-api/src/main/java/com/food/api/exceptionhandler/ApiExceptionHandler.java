dc0936fa7e25fefc3158134c5bfa030b3311f021
rondinelli-oliveira <rondinelliads@gmail.com>
2020-07-24 20:25:33 -0300

    Customizando exception handlers de ResponseEntityExceptionHandler.

--- food-api/src/main/java/com/food/api/exceptionhandler/ApiExceptionHandler.java
+++ food-api/src/main/java/com/food/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,6 +3,7 @@ package com.food.api.exceptionhandler;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -15,6 +16,22 @@ import com.food.domain.exception.NegocioException;
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+		String detail = "O corpo da requisição está inválido. Verifique erro de sintaxe.";
+		
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		
+		// Na aula ficou passando uma nova instância de HttpHeaders, mas o ideal
+		// é repassar o "headers" que recebemos como argumento do método
+		// return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+		
+		return handleExceptionInternal(ex, problem, headers, status, request);
+	}
+	
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
 	public ResponseEntity<?> handleEntidadeNaoEncontradaException(
 			EntidadeNaoEncontradaException ex, WebRequest request) {
--- food-api/src/main/java/com/food/api/exceptionhandler/ProblemType.java
+++ food-api/src/main/java/com/food/api/exceptionhandler/ProblemType.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
+	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
 	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");
@@ -13,7 +14,7 @@ public enum ProblemType {
 	private String uri;
 	
 	ProblemType(String path, String title) {
-		this.uri = "https://algafood.com.br" + path;
+		this.uri = "https://food.com.br" + path;
 		this.title = title;
 	}
 	
