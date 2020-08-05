8d0cb9d142956a825b9670c01ea455adc43042a4
rondinelli-oliveira <rondinelliads@gmail.com>
2020-08-04 22:38:49 -0300

    Criando e rodando um teste de integração com Spring Boot, JUnit e AssertJ.

--- food-api/src/test/java/com/food/api/FoodApiApplicationTests.java
+++ food-api/src/test/java/com/food/api/FoodApiApplicationTests.java
@@ -1,16 +1,16 @@
 package com.food.api;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class FoodApiApplicationTests {
+public class FoodApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
