16ebc944ca1a015070415befe15bd436f2bacf0e
rondinelli-oliveira <rondinelliads@gmail.com>
2022-03-25 20:36:17 -0300

    3.19. Propriedade nullable de @Column e @JoinColumn.

--- food-api/src/main/java/com/evolution/food/api/domain/model/Restaurante.java
+++ food-api/src/main/java/com/evolution/food/api/domain/model/Restaurante.java
@@ -16,12 +16,14 @@ public class Restaurante {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
-    
+
+    @Column(nullable = false)
     private BigDecimal taxaFrete;
 
     @ManyToOne
-    @JoinColumn(name = "cozinha_id")
+    @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
 
 }
