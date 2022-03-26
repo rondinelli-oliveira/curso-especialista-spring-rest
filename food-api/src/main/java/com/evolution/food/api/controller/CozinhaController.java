c569101f623252a95d4c5cb18e5e1f2af816b93b
rondinelli-oliveira <rondinelliads@gmail.com>
2022-03-25 23:04:35 -0300

    4.10. Modelando e requisitando um Collection Resource com GET.

---
+++ food-api/src/main/java/com/evolution/food/api/controller/CozinhaController.java
@@ -0,0 +1,23 @@
+package com.evolution.food.api.controller;
+
+import com.evolution.food.api.domain.model.Cozinha;
+import com.evolution.food.api.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return cozinhaRepository.listar();
+    }
+}
