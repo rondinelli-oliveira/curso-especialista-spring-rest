56349458519f00c5e0645f926827d65c685d7549
rondinelli-oliveira <rondinelliads@gmail.com>
2022-03-25 23:33:58 -0300

    4.11. Desafio: collection resource de estados.

---
+++ food-api/src/main/java/com/evolution/food/api/controller/EstadoController.java
@@ -0,0 +1,23 @@
+package com.evolution.food.api.controller;
+
+import com.evolution.food.api.domain.model.Estado;
+import com.evolution.food.api.domain.repository.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    @GetMapping
+    public List<Estado> listar() {
+        return estadoRepository.listar();
+    }
+}
