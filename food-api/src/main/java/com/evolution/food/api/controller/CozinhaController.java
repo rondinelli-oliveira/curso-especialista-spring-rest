7195c1454ccd2431783caf86c2b461e2a09bf458
rondinelli-oliveira <rondinelliads@gmail.com>
2022-03-27 21:07:11 -0300

    4.13. Implementando content negotiation para retornar JSON ou XML.

--- food-api/src/main/java/com/evolution/food/api/controller/CozinhaController.java
+++ food-api/src/main/java/com/evolution/food/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.evolution.food.api.controller;
 import com.evolution.food.api.domain.model.Cozinha;
 import com.evolution.food.api.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping
+    @GetMapping(MediaType.APPLICATION_JSON_VALUE)
     public List<Cozinha> listar() {
         return cozinhaRepository.listar();
     }
