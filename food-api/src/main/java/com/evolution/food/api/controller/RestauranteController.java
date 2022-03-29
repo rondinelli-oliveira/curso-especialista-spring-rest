16ad19be6803b3585c23ee5a229cd97e4e0d5631
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-03-29 12:16:07 -0300

    4.29. Desafio: modelando e implementando a consulta de recursos de restaurantes.

---
+++ food-api/src/main/java/com/evolution/food/api/controller/RestauranteController.java
@@ -0,0 +1,36 @@
+package com.evolution.food.api.controller;
+
+import com.evolution.food.api.domain.model.Restaurante;
+import com.evolution.food.api.domain.repository.RestauranteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/restaurantes")
+public class RestauranteController {
+
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @GetMapping
+    public List<Restaurante> listar() {
+        return restauranteRepository.listar();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Restaurante> buscar(@PathVariable Long id) {
+        Restaurante restaurante = restauranteRepository.buscar(id);
+
+        if (restaurante != null) {
+            return ResponseEntity.ok(restaurante);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
