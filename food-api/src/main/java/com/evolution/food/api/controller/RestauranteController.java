f2bb6f9596fc8b2d713f44b5caffe8d297ba64d5
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-03-29 17:04:28 -0300

    4.30. Modelando e implementando a inclusão de recursos de restaurantes.

--- food-api/src/main/java/com/evolution/food/api/controller/RestauranteController.java
+++ food-api/src/main/java/com/evolution/food/api/controller/RestauranteController.java
@@ -1,13 +1,13 @@
 package com.evolution.food.api.controller;
 
+import com.evolution.food.api.domain.exception.EntidadeNaoEncontradaException;
 import com.evolution.food.api.domain.model.Restaurante;
 import com.evolution.food.api.domain.repository.RestauranteRepository;
+import com.evolution.food.api.domain.service.RestauranteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +17,9 @@ public class RestauranteController {
 
     @Autowired
     private RestauranteRepository restauranteRepository;
+
+    @Autowired
+    private RestauranteService restauranteService;
 
     @GetMapping
     public List<Restaurante> listar() {
@@ -31,6 +34,19 @@ public class RestauranteController {
             return ResponseEntity.ok(restaurante);
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante) {
+        try {
+            restaurante = restauranteService.salvar(restaurante);
+
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(restaurante);
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.badRequest()
+                    .body(e.getMessage());
+        }
     }
 
 }
