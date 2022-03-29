136221bfd4c3ad035f6d24a25a82adb3b7351504
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-03-29 11:00:00 -0300

    4.28. Refatorando a exclusão de cozinhas para usar domain services.

--- food-api/src/main/java/com/evolution/food/api/domain/service/CozinhaService.java
+++ food-api/src/main/java/com/evolution/food/api/domain/service/CozinhaService.java
@@ -1,8 +1,12 @@
 package com.evolution.food.api.domain.service;
 
+import com.evolution.food.api.domain.exception.EntidadeEmUsoException;
+import com.evolution.food.api.domain.exception.EntidadeNaoEncontradaException;
 import com.evolution.food.api.domain.model.Cozinha;
 import com.evolution.food.api.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,5 +17,17 @@ public class CozinhaService {
 
     public Cozinha salvar(Cozinha cozinha) {
         return cozinhaRepository.salvar(cozinha);
+    }
+
+    public void excluir(Long id) {
+        try {
+            cozinhaRepository.remover(id);
+        }catch (EmptyResultDataAccessException e) {
+            throw new EntidadeNaoEncontradaException(
+                    String.format("Não existe cadastro de cozinha com o código: %d.", id));
+        }catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoException(
+                    String.format("Cozinha de código %d não pode ser excluída, pois está em uso.", id));
+        }
     }
 }
