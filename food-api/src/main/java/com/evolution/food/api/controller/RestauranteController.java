c15aacc10f60b488012da97bd1559193b2e1fb7f
rondinelli-oliveira <rondinelliads@gmail.com>
2022-03-29 22:17:04 -0300

    4.34. Finalizando a atualização parcial com a API de Reflections do Spring.

--- food-api/src/main/java/com/evolution/food/api/controller/RestauranteController.java
+++ food-api/src/main/java/com/evolution/food/api/controller/RestauranteController.java
@@ -4,12 +4,15 @@ import com.evolution.food.api.domain.exception.EntidadeNaoEncontradaException;
 import com.evolution.food.api.domain.model.Restaurante;
 import com.evolution.food.api.domain.repository.RestauranteRepository;
 import com.evolution.food.api.domain.service.RestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
@@ -85,9 +88,19 @@ public class RestauranteController {
         return atualizar(id, restauranteAtual);
     }
 
-    private void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino) {
-        camposOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-            System.out.println(nomePropriedade + " = " + valorPropriedade);
+    private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+
+        dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+            Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+            field.setAccessible(true);
+
+            Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+
+//			System.out.println(nomePropriedade + " = " + valorPropriedade + " = " + novoValor);
+
+            ReflectionUtils.setField(field, restauranteDestino, novoValor);
         });
     }
 
