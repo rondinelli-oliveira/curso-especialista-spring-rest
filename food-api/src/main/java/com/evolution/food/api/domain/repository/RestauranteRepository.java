29e1b4774c98dd5e4062fcc899fdb18d7964a93c
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-01 15:02:09 -0300

    5.10. Externalizando consultas JPQL para um arquivo XML.

--- food-api/src/main/java/com/evolution/food/api/domain/repository/RestauranteRepository.java
+++ food-api/src/main/java/com/evolution/food/api/domain/repository/RestauranteRepository.java
@@ -17,7 +17,7 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 
 //    List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
 
-    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+//    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
     List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
 
     Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
