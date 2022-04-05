abbf681e0870c36516d4748c1f0ce429dfea89b4
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-05 17:06:26 -0300

    6.7. Desafio: mapeando relacionamento muitos-para-um.

---
+++ food-api/src/main/java/com/evolution/food/api/domain/model/Produto.java
@@ -0,0 +1,31 @@
+package com.evolution.food.api.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    private BigDecimal preco;
+
+    private Boolean ativo;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Restaurante restaurante;
+}
