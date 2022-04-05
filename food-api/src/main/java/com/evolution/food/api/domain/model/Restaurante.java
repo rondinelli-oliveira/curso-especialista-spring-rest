688ed9719ff95e104fd1281cd316df3e4efffe78
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-05 15:25:11 -0300

    6.4. Mapeando classes incorporáveis com @Embedded e @Embeddable.

--- food-api/src/main/java/com/evolution/food/api/domain/model/Restaurante.java
+++ food-api/src/main/java/com/evolution/food/api/domain/model/Restaurante.java
@@ -29,11 +29,16 @@ public class Restaurante {
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
 
+    @Embedded
+    private Endereco endereco;
+
     @JsonIgnore
     @ManyToMany
     @JoinTable(name = "restaurante_forma_pagamento",
                 joinColumns = @JoinColumn(name = "restaurante_id"),
     inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
     private List<FormaPagamento> formasPagamento = new ArrayList<>();
+
+
 
 }
