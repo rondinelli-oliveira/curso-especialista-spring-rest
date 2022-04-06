3d11712b6bfac152e4b9b567e062afdbb1114b3b
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-06 10:38:09 -0300

    6.14. Resolvendo o Problema do N+1 com fetch join na JPQL.

--- food-api/src/main/java/com/evolution/food/api/domain/model/Restaurante.java
+++ food-api/src/main/java/com/evolution/food/api/domain/model/Restaurante.java
@@ -29,9 +29,9 @@ public class Restaurante {
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
-    @JsonIgnore
+//    @JsonIgnore
 //    @JsonIgnoreProperties("hibernateLazyInitializer")
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne //(fetch = FetchType.LAZY)
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
 
@@ -49,8 +49,8 @@ public class Restaurante {
     @Column(nullable = false, columnDefinition = "datetime(0)")
     private LocalDateTime dataAtualizacao;
 
-    @JsonIgnore
-    @ManyToMany(fetch = FetchType.EAGER)
+//    @JsonIgnore
+    @ManyToMany //(fetch = FetchType.EAGER)
     @JoinTable(name = "restaurante_forma_pagamento",
                 joinColumns = @JoinColumn(name = "restaurante_id"),
     inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
